e5626c53420a19ab280e138c9e29854b347a9521
chanuka <chanuucsc@gmail.com>
2024-03-18 19:00:59 +0530

    Add files via upload

---
+++ Eureka-discover-server(Java)/src/main/java/com/cba/core/eureka/AuthServerApplication.java
@@ -0,0 +1,16 @@
+package com.cba.core.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
+
