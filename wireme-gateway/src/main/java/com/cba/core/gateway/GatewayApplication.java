ced0d58456bb32ebb0f6b3b86c1090a8408ff13a
bitchanuka <chanuucsc@gmail.com>
2024-03-20 21:42:22 +0530

    resolve gitignore

--- wireme-gateway/src/main/java/com/cba/core/gateway/GatewayApplication.java
+++ wireme-gateway/src/main/java/com/cba/core/gateway/GatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class WireMeGatewayApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WireMeGatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
